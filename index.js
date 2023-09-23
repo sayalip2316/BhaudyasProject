f9ced8d259a552a031f4dbbbca2bffd6f7a2be99
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2023-09-23 21:06:01 +0530

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const app=express();
+const {connection}=require("./config/db");
+const {userRouter}=require("./routes/user.routes");
+const cors=require("cors");
+const dotenv=require("dotenv");
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/user",userRouter)
+
+
+app.listen(process.env.PORT,async(req,res)=>{
+    try {
+       await connection
+       console.log("Connected to db") 
+       console.log(`Server is listening on port ${process.env.PORT}`)
+    } catch (error) {
+        console.log(error)
+    }
+})
